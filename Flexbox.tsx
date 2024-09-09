24afb484a252210bda3fbc307f86c77c3a0b2fdf
stenlyadam <stenly.adam@unklab.ac.id>
2024-09-09 14:16:50 +0800

    flexbox cont.

--- Flexbox.tsx
+++ Flexbox.tsx
@@ -3,17 +3,36 @@ import React from 'react';
 
 const Flexbox = () => {
   return (
-    <View>
-      <View style={styles.redBox} />
-      <View style={styles.greenBox} />
-      <View style={styles.blueBox} />
-    </View>
+    <>
+      <View style={styles.topContainer} />
+      <View style={styles.container}>
+        <View style={styles.redBox} />
+        <View style={styles.greenBox} />
+        <View style={styles.blueBox} />
+      </View>
+      <View style={styles.bottomContainer} />
+    </>
   );
 };
 
 export default Flexbox;
 
 const styles = StyleSheet.create({
+  topContainer: {
+    backgroundColor: 'purple',
+    flex: 1,
+  },
+  container: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: 'yellow',
+  },
+  bottomContainer: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
   redBox: {
     height: 100,
     width: 100,
